5b724e3e0ecdbd762133d401414037a784d33594
yanichik <brunshte@gmail.com>
2021-04-22 06:39:51 -0700

    mongoose setup

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
+/*START INCLUSIONS*/
 const express = require('express');
 const app = express();
-// ejs-mate view engine to include layout, partial, & block template functions for ejs view engine
-const ejsMate = require('ejs-mate');
+const ejsMate = require('ejs-mate');  // includes layout, partial, & block template functions for view engine
 const path = require('path');
-// port defined in .env file or localhost:3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;  // port defined in .env file or localhost:3000
+const mongoose = require('mongoose');
+/*END INCLUSIONS*/
 
-app.engine('ejs', ejsMate);	// sets the ejsMate function to render all ejs files
-app.set('view engine', 'ejs'); // sets the view engine to ejs
-app.set('views', path.join(__dirname, 'views')); // sets default directory for all static views
+/*START MONGOOSE SETUP*/
+mongoose.connect('mongodb://localhost/hockey-camp', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false});
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log("Mongoose connected")
+});
+/*END MONGOOSE SETUP*/
 
-// All app.use cases
-app.use(express.static(path.join(__dirname, 'public')));	// sets default directory 'public' to serve all static assets
+/*START SETS*/
+app.engine('ejs', ejsMate);  // sets the ejsMate function to render all ejs files
+app.set('view engine', 'ejs');  // sets the view engine to ejs
+app.set('views', path.join(__dirname, 'views'));  // sets default directory for all static views
+/*END SETS*/
 
+/*START USES*/
+app.use(express.static(path.join(__dirname, 'public')));  // sets default directory 'public' to serve all static assets
+/*END USES*/
+
+/*START ROUTES*/
 app.get('/', (req, res, next)=>{
-	// res.send('welcome camp seakers!')
+	// res.send('welcome camp seakers!');
 	res.render('home', {'camp': 'heartland hockey camps'});
 })
+/*END ROUTES*/
 
+/*START LISTEN @ ROUTER*/
 app.listen(port, ()=>{
 	console.log(`Listening on Port ${port}`);
 })
+/*END LISTEN @ ROUTER*/
