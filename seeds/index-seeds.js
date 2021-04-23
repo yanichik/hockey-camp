f0951f50a2e3e9231e2b0f63b35ee1836c14fda8
yanichik <brunshte@gmail.com>
2021-04-23 00:10:30 -0700

    seeding mock data

--- seeds/index-seeds.js
+++ seeds/index-seeds.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Camp = require('../models/camp');
-const pics = require('./pics-seeds');
+const picsArray = require('./pics-seeds');
 
 /*START MONGOOSE SETUP*/
 mongoose.connect('mongodb://localhost/hockey-camp', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true,
-	useFindAndModify: false});
+	useFindAndModify: false
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -26,6 +27,33 @@ db.once('open', function() {
 // });
 /*END INSERT ONE*/
 
-/*START INSERT MANY*/
-const insertPics = Camp.insertMany(pics);
-/*END INSERT MANY*/
+/*START SEED*/
+const seedCamps = async () =>{
+	const picsSize = picsArray.length;
+	for (let i = 0; i < picsSize; i++){
+		const newCamp = new Camp({pics: picsArray[i].pic});
+
+		/* THIS SAVE METHOD RETURNS "MongoError: Topology is closed, please connect" */
+		await newCamp.save(function(e, someCamp){
+			if(e) return console.log("Something went wrong :(" + e);
+			console.log(`Successfully added ${someCamp}`);
+		})
+
+		/* this save method works without fault */
+		// await newCamp.save();
+	}
+}
+
+/* this close method works without fault */
+/*seedCamps().then(() => {
+	mongoose.connection.close();
+})
+.catch(e => {console.log(e)})*/
+
+/* THIS DISCONNECT METHOD RETURNS "TypeError: mongoose.connection.disconnect is not a function" */
+seedCamps().then(() => {
+	mongoose.connection.disconnect();
+})
+.catch(e => {console.log(e)})
+
+/*END SEED*/
