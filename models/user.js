281323647ac691432be31b45752ccb9337053696
yanichik <brunshte@gmail.com>
2021-04-25 14:15:59 -0700

    create userSchema in models

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	firstName: String,
+	lastName: String,
+	userName: String,
+	email: String,
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
